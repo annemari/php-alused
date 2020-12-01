5f7a70225517890c83c5bf0b29299c70584f576a
Anne-Mari <annemari.eensaar@gmail.com>
2020-12-01 09:30:59 +0200

    Ülesanne 1 lahendus

---
+++ yl1.php
@@ -0,0 +1,10 @@
+<?php
+// ülesanne 1 Anne-Mari Eensaar 01/12/2020
+$nimi = "Anne-Mari";
+$synniaasta = "1992";
+$tahtkuju = "vähk";
+echo "Minu nimi on ".$nimi.", ma sündisin aastal ".$synniaasta." ja mu tähtkuju on ".$tahtkuju."<br>";
+echo "\"It’s My Life\" – Dr. Alban<br>";
+echo "(\\(\\<br>";
+echo "( -.-)<br>";
+echo "o_(\")(\")<br>";
